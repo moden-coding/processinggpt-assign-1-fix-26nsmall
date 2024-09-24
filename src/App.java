a5bc996735ff1fa87b5a94273216094c7d97813c
26nsmall <26nsmall@gannacademy.org>
2024-09-24 09:40:17 -0400

    done

--- src/App.java
+++ src/App.java
@@ -4,4 +4,24 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+    public void setup(){
+        background(200);
+    }
+
+    public void settings(){
+        size(400,400);
+    }
+
+    public void draw(){
+        strokeWeight(16);
+        stroke(0,0, 255);
+        fill(255, 0, 0);
+        rect(0, 0, 100, 200);
+        stroke(255, 0, 0);
+        strokeWeight(20);
+        fill(0, 0, 255);
+        ellipse(400, 400, 100, 100);
+        strokeWeight(5);
+        line(90, 190, 365, 365);
+    }
 }
